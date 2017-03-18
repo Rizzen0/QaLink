aa0fac43fc5de889fa506a6d6920a9c7c2973418
nicolas.godfrin@epitech.eu <nicolas.godfrin@epitech.eu>
2017-03-18 18:43:30 +0100

    ajout des routes questions patients

--- api/index.js
+++ api/index.js
@@ -1,33 +1,5 @@
-var express = require('require');
-var router = express.Router();
-
-router.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-router.get('/', function (req, res, next) {
-    var session = req.session;
-
-    res.send('<p>QA Link API.</p>');
-});
-
-var user = require('./question.js');
-
-router.route('/question')
-    .get(question.apiGET)
-
-module.exports = router;
+var express = require('express');
+var app = express();
+var api = require('./route.js');
+app.use('/api', api);
+app.listen(3000);
