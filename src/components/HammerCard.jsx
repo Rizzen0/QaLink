23ea6ce0e6185cf5b243d43ffcf3d2f05a902736
Adrien Muzika <adrien.muzika@epitech.eu>
2017-03-18 14:30:37 +0100

    swipe working :3

--- src/components/HammerCard.jsx
+++ src/components/HammerCard.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import Hammer, { handleTap, handleSwipe } from 'react-hammerjs'
+import Hammer from 'react-hammerjs'
 import { Card } from 'semantic-ui-react'
 
-const CardExampleIndividualCardProps = () => (
-  <Card
-    header='Vous sentez-vous bien ?'
-  />
+const items = [{
+    header: 'Project Report - April',
+    description: 'Leverage agile frameworks to provide a robust synopsis for high level overviews.',
+    meta: 'ROI: 30%',
+  }]
+
+const CardTest = () => (
+  <Card.Group items={items} />
 )
 
 export default class HammerCard extends React.Component {
+  constructor(){
+    super();
+
+    this.handleSwipe = function(){
+      alert('Swipe');
+    }
+  }
+
   render() {
     return (
       <div>
-        <Hammer onTap={handleTap} onSwipe={handleSwipe}><CardExampleIndividualCardProps/></Hammer>
+        <Hammer onSwipe={this.handleSwipe}><CardTest/></Hammer>
       </div>
     )
   }
