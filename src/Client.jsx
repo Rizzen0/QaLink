b98c4043c0ae9cc5e8401ba046fe676935078d4a
Oprax <oprax@me.com>
2017-03-18 21:08:12 +0100

    Doctor Page with charts

--- src/Client.jsx
+++ src/Client.jsx
@@ -5,6 +5,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './components/App.jsx';
 import PatientPage from './components/PatientPage.jsx';
 import DoctorPage from './components/DoctorPage.jsx'
+import DoctorPatientView from './components/DoctorPatientView.jsx'
 
 const app = document.getElementById('app');
 
@@ -13,6 +14,7 @@ ReactDOM.render(
                   <Route path="/" component={App}>
                     <IndexRoute component={PatientPage}/>
                     <Route path="/DoctorPage" component={DoctorPage}/>
+                    <Route path="/DoctorPage/patient/:id" component={DoctorPatientView}/>
                   </Route>
                 </Router>
                 , app);
