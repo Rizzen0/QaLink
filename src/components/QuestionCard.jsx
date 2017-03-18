4f4dfc8fde8f5230ceb19416828e057d203d6b12
Thibault Miranda de Oliveira <thibaultmiranda@outlook.com>
2017-03-18 19:11:29 +0100

    Basic swipe

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,27 +1,45 @@
 import React from 'react'
-import Hammer from 'react-hammerjs'
+import HamComp from 'react-hammerjs'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
 import { Card } from 'semantic-ui-react'
 
 export class QuestionCard extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
-    this.handleSwipe = function(){
-      alert('Swipe');
+    this.state = {
+      index: 0
+    };
+    this.handleSwipe = function(e){
+      if (e.direction == Hammer.DIRECTION_LEFT) {
+        this.setState({index: 1});
+        console.log('left');
+      }
+      else if (e.direction == Hammer.DIRECTION_RIGHT) {
+        console.log('right');
+      }
     }
   }
 
-  render() {
-    return (
-            <div>
-              <Hammer direction={Hammer.DIRECTION_LEFT} onSwipe={this.handleSwipe}>
-                <Card
-                  header='Elliot Baker'
-                  meta='Friend'
-                  description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-                />
-              </Hammer>
-            </div>
-            )
-  }
+    render() {
+      if (this.state.index == 1) { 
+        return (
+                <div>lol</div>
+                )
+      }
+      else {
+        return (
+                <div>
+                  <HamComp onSwipe={this.handleSwipe.bind(this)}>
+                    <Card
+                      header='Elliot Baker'
+                      meta='Friend'
+                      description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                    />
+                  </HamComp>
+                </div>
+                )
+      }
+    }
 }
