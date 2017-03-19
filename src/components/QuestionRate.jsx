d1bb13425a0ce6bd66ced52fdbd3d061958b7fcf
Adrien Muzika <adrien.muzika@epitech.eu>
2017-03-19 02:58:48 +0100

    now can retrieve good rating

--- src/components/QuestionRate.jsx
+++ src/components/QuestionRate.jsx
@@ -23,7 +23,7 @@ export default class QuestionRate extends React.Component {
     };
 
     this.getAnswer = function(rating){
-        console.log('button pressed for ' + rating);
+      console.log(rating);
     };
 
     this.state = { rating: 2 };
@@ -38,7 +38,7 @@ export default class QuestionRate extends React.Component {
         <br/>
         <input type='range' min={1} max={3} value={rating} onChange={this.handleChange}/>
         <br/><br/>
-        <Button color="blue" circular={true} onClick={this.getAnswer} primary>Continuer</Button>
+        <Button color="blue" circular={true} onClick={() => this.getAnswer(rating)} primary>Continuer</Button>
       </div>
     );
   }
