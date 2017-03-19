c229d442ac99dabc00281289421b51447ea61535
Thibault Miranda de Oliveira <thibaultmiranda@outlook.com>
2017-03-19 01:15:44 +0100

    new archi for process

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -7,25 +7,14 @@ import { Card } from 'semantic-ui-react'
 export class QuestionCard extends React.Component {
   constructor() {
     super();
-
-    this.handleSwipe = function (e) {
-      if (e.direction == Hammer.DIRECTION_LEFT) {
-        console.log('left');
-      }
-      else if (e.direction == Hammer.DIRECTION_RIGHT) {
-        console.log('right');
-      }
-    }
   }
 
   render() {
     return (
             <div className="questionCard">
-              <HamComp onSwipe={this.handleSwipe.bind(this)}>
+              <HamComp onSwipe={this.props.handleSwipe.bind(this.props.me)}>
                 <Card
-                  header='Elliot Baker'
-                  meta='Friend'
-                  description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                  header={this.props.question}
                 />
               </HamComp>
             </div>
